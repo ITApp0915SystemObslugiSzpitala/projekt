d54e53bb8add1da3afa39d89833646119c9b2ab5
TPapior <tomasz.papior@gmail.com>
2015-06-08 22:26:58 +0200

    wersja 9
    naprawiona nawigacja

---
+++ hospital9/hospital/patientaction.php
@@ -0,0 +1,116 @@
+
+
+
+	<?php
+	session_start();
+
+
+
+
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+$db =new mysqli('localhost', 'root', 'haslo123', 'hospital_dev')or die('Błąd !: ' . mysql_error());
+
+            if ($_POST["button"]=="Dokumenty"){
+			$idp=$_POST['patient_id'];
+			
+		//header ('Location: patientdocs.php?id='.$_POST['patient_id'].'');
+		
+		}
+		
+		
+if ($_POST["button"]=="Aktualizuj"){
+
+ 
+  $db->query("UPDATE patients  SET forename = '".$_POST['ud_forename']."', surname = '".$_POST['ud_surname']."', gender = '".$_POST['ud_gender']."',  birth_place = '".$_POST['ud_birth_place']."', modified_by_user_id = '".$_SESSION['id']."'  where patient_id = '".$_POST['ud_patient_id']."';");
+
+$_SESSION['message'] = '<div class="alert alert-success" role="alert">Zaktualizowano
+</div>';
+}
+
+
+if ($_POST["button"]=="Dodaj dokument"){
+ 
+
+$db->query("INSERT INTO identity_documents (document_type_id, document_number) values ('".$_POST['doctype']."', '".$_POST['doc_nr']."');");
+$iddokumentu = $db->insert_id;
+$db->query("INSERT INTO patient_identity_docs (patient_id, identity_document_id, default_flg) values ('".$_GET['id']."', '".$iddokumentu."', '0' );");
+ $_SESSION['message'] = '<div class="alert alert-success" role="alert">Dokument dodany do bazy . </div>';
+ header ('Location: patientdocs.php?id='.$_GET['id'].'');}
+ if ($_POST["button"]=="Dodaj adres"){
+ 
+$db->query("INSERT INTO addresses (street_name, street_number, city,  postal_code, country, modified_by_user_id, created_by_user_id, created_on_date, address_type_id) values ('".$_POST['street_name']."', '".$_POST['street_number']."', '".$_POST['city']."', '".$_POST['postal_code']."', '".$_POST['country']."',  '".$_SESSION['id']."', '".$_SESSION['id']."', '".date('Y-m-d H:m:s')."', '3') ;");
+$idadresu = $db->insert_id;
+$db->query("INSERT INTO patient_addresses (address_id, patient_id) values ('".$idadresu."', '".$_GET['id']."' );");
+ $_SESSION['message'] = '<div class="alert alert-success" role="alert">Adres dodany do bazy . </div>';
+ header ('Location: patientaddresses.php?id='.$_GET['id'].'');
+ }
+if ($_POST["button"]=="Wstaw"){
+ $db->query("INSERT INTO patients (forename, surname,  gender, birth_place, birth_date, modified_by_user_id, created_by_user_id, created_on_date ) values ('".$_POST['forename']."', '".$_POST['surname']."', '".$_POST['gender']."', '".$_POST['birth_place']."', '".$_POST['birth_date']."', '".$_SESSION['id']."', '".$_SESSION['id']."', '".date('Y-m-d H:m:s')."');");
+
+$idpacjenta = $db->insert_id;
+$db->query("INSERT INTO addresses (street_name, street_number, city,  postal_code, country, modified_by_user_id, created_by_user_id, created_on_date, address_type_id) values ('".$_POST['street_name']."', '".$_POST['street_number']."', '".$_POST['city']."', '".$_POST['postal_code']."', '".$_POST['country']."',  '".$_SESSION['id']."', '".$_SESSION['id']."', '".date('Y-m-d H:m:s')."', '3') ;");
+$idadresu = $db->insert_id;
+$db->query("INSERT INTO patient_addresses (address_id, patient_id) values ('".$idadresu."', '".$idpacjenta."' );");
+//echo "INSERT INTO identity_documents (document_type_id, document_number) values ('".$_POST['doctype']."', '".$_POST['doc_nr']."');";
+$db->query("INSERT INTO identity_documents (document_type_id, document_number) values ('".$_POST['doctype']."', '".$_POST['doc_nr']."');");
+$iddokumentu = $db->insert_id;
+$db->query("INSERT INTO patient_identity_docs (patient_id, identity_document_id, default_flg) values ('".$idpacjenta."', '".$iddokumentu."', '1' );");
+//echo "INSERT INTO patient_identity_docs (patient_id, identity_document_id, default_flg) values ('".$idpacjenta."', '".$iddokumentu."', '1' );";
+ $_SESSION['message'] = '<div class="alert alert-success" role="alert">Pacjent dodany do bazy . </div>';
+header ('Location: mainpage.php');
+ }
+ elseif ($_POST["button"]=="Przyjmij"){
+ $result = $db->query("SELECT patient_stays.* FROM patient_stays where patient_id=".$_POST['patient_id']." AND date_to IS NULL");
+echo $result->num_rows;
+if ($result->num_rows!=0){
+{ $_SESSION['message'] = '<div class="alert alert-danger" role="alert">Ten pacjent znajduje się obecnie w szpitalu</div>'; }
+header ('Location: mainpage.php');}
+
+else {
+ $db->query("INSERT INTO patient_stays (patient_id, date_from,  created_by_user_id, created_on_date, stay_code ) values ('".$_POST['patient_id']."', '".$_POST['date_from']."', '".$_SESSION['id']."', '".date('Y-m-d H:m:s')."','".$_POST['stay_code']."');");
+$stay = $db->insert_id;
+$db->query("INSERT INTO patient_records (patient_id, created_by_user_id, created_on_date, ward_id, record_code ) values ('".$_POST['patient_id']."', '".$_SESSION['id']."', '".date('Y-m-d H:m:s')."','".$_POST['ward_id']."','".$_POST['record_code']."');");
+$rec = $db->insert_id;
+$db->query("INSERT INTO record_items (patient_stay_id, date_from, patient_record_id ) values ('".$stay."', '".$_POST['date_from']."','".$rec."');");
+$_SESSION['message'] = '<div class="alert alert-success" role="alert">Pacjent o id: '.$_POST['patient_id'].' został przyjęty na oddział '.$_POST['ward_id'].' . </div>';
+header ('Location: mainpage.php');}
+}
+if ($_POST["button"]=="Wypisz"){
+ $db->query("UPDATE patient_stays SET date_to='".date('Y-m-d H:m:s')."'  where patient_stay_id= '".$_POST['patient_stay_id']."' AND date_to IS NULL");
+$_SESSION['message'] = '<div class="alert alert-success" role="alert">Pacjent wypisany. </div>';
+header ('Location: mainpage.php');
+ }
+ if ($_POST["button"]=="Dodaj karte"){
+ $db->query("INSERT INTO patient_records (patient_id, created_by_user_id, created_on_date, ward_id, record_code ) values ('".$_GET['patient']."', '".$_SESSION['id']."', '".date('Y-m-d H:m:s')."','".$_POST['ward_id']."','".$_POST['record_code']."');");
+$rec = $db->insert_id;
+$db->query("INSERT INTO record_items (patient_stay_id, date_from, patient_record_id ) values ('".$_GET['stayid']."', '".$_POST['date_from']."','".$rec."');");
+$_SESSION['message'] = '<div class="alert alert-success" role="alert">Pacjent o id: '.$_GET['patient'].' został przyjęty na oddział '.$_POST['ward_id'].' . </div>';
+header ('Location: patientstays.php?patient='.$_GET['patient'].'&id='.$_GET['stayid'].'');
+ }
+ if ($_POST["button"]=="Dodaj operacje"){
+ 
+$db->query("INSERT INTO patient_operations (operation_code, patient_id, record_item_id, room_id, date_from, created_by_user_id ) values ('".$_POST['operation_code']."', '".$_GET['patient']."','".$_GET['recordid']."','".$_POST['room_id']."','".$_POST['date_from']."', '".$_SESSION['id']."');");
+//echo "INSERT INTO patient_operations (operation_code, patient_id, record_item_id, room_id, date_from, created_by_user_id ) values ('".$_POST['operation_code']."', '".$_GET['patient']."','".$_GET['recordid']."','".$_POST['room_id']."','".$_POST['date_from']."', '".$_SESSION['id']."');";
+$_SESSION['message'] = '<div class="alert alert-success" role="alert">Pacjent o id: '.$_GET['patient'].' ma dodaną operację. </div>';
+header ('Location: patientstays2.php?patient='.$_GET['patient'].'&id='.$_GET['stayid'].'&recordid='.$_GET['recordid'].'');
+ }
+  if ($_POST["button"]=="Dodaj diagnoze"){
+ 
+$db->query("INSERT INTO comments (comment_text, created_by_user_id, created_on_date ) values ('".$_POST['comment']."', '".$_SESSION['id']."','".date('Y-m-d H:m:s')."');");
+$kom = $db->insert_id;
+$db->query("INSERT INTO diagnoses (record_item_id, diagnose_code, comment_id, created_by_user_id, created_on_date ) values ('".$_GET['recordid']."', '".$_POST['diagnose_code']."', '".$kom."', '".$_SESSION['id']."','".date('Y-m-d H:m:s')."');");
+$_SESSION['message'] = '<div class="alert alert-success" role="alert">Pacjent o id: '.$_GET['patient'].' ma dodaną diagnozę. </div>';
+
+header ('Location: patientstays2.php?patient='.$_GET['patient'].'&id='.$_GET['stayid'].'&recordid='.$_GET['recordid'].'');
+ }
+$db->close();
+
+
+?>
+    
+            
+           
+
+
+
---
+++ hospital9/hospital/useredit.php
@@ -0,0 +1,182 @@
+<?php
+if ($_POST["button"]=="Wybierz"){
+    header ('Location: userinfo.php?id='.$_POST['user_id'].'');
+    }
+session_start();
+	require_once ('log_check.php');	 
+		 $message=$_SESSION['message'];
+		 $_SESSION['message']=NULL;
+		 
+		  ?>
+<!DOCTYPE html>
+<html lang="pl_PL">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>System Obsługi Szpitala</title>
+ 
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+ 
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+<form action="useraction.php" method=post ">
+  <style type="text/css">
+
+  body {
+    padding-top: 70px;
+  }
+    </style>
+    
+    
+  <div class="container">
+
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <div class="container">
+    <!-- Grupowanie "marki" i przycisku rozwijania mobilnego menu -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Rozwiń nawigację</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="mainpage.php">System Obsługi Szpitala</a>
+    </div>
+ 
+    <!-- Grupowanie elementów menu w celu lepszego wyświetlania na urządzeniach moblinych -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#"></a></li>
+       <li><a href="przyjmij.php">Przyjmij</a></li>
+        
+        
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pacjent<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="patientprofile.php">Dodaj</a></li>
+            <li><a href="patientlist.php">Edytuj</a></li>
+            
+            <li class="divider"></li>
+            <li><a href="patientlist.php">Wyszukaj</a></li>
+            
+        
+          </ul>
+        </li>
+
+        
+
+
+      </ul>
+
+        <ul class="nav navbar-nav">
+        <li class="active"><a href="#"></a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Personel<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="adduser.php">Dodaj</a></li>
+            <li><a href="userlist.php">Edytuj</a></li>
+            <li><a href="#">Usuń</a></li>
+            
+            
+        
+          </ul>
+        </li>
+
+       
+       
+    <li ><a href="adminlogout.php">Wyloguj: <?php echo $_SESSION['login'],' ', $_SESSION['surname']; ?></a></li>
+      </ul>
+
+
+       
+
+      </ul>
+
+
+
+
+      
+      
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+  
+
+
+
+
+
+</div>
+
+
+
+
+<div class="container theme-showcase" role="main">
+
+      <div class="jumbotron">
+        <h3>Przyjmij pacjenta</h3>
+		<input type="hidden" name="searchid" id="searchid" value="<?php echo $_POST['user_id'] ?>" />
+		<?php
+		ini_set('display_errors',0);
+		
+		
+		error_reporting(E_ALL);
+		$db =new mysqli('localhost', 'root', 'haslo123', 'hospital_dev')or die('Błąd !: ' . mysql_error());
+		
+		$result = $db->query("SELECT users.* FROM users where user_id=".$_POST['user_id']);
+		while($row = $result->fetch_assoc()){
+		echo '
+		
+	<div class="input-group">
+  <span class="input-group-addon">ID pacjenta</span>
+  <input type="text" id="user_id" name="user_id" class="form-control" value="'.$_POST['user_id'].'" readonly></div>
+   <div class="input-group">
+  <span class="input-group-addon">Kod<span style="color: red">*</span></span>
+  <input type="text" name="stay_code" id="stay_code" class="form-control" placeholder="Wpisz kod wizyty"  autocomplete="off" data-toggle="popover" onClick=pop()  data-content="Wprowadź kod wizyty"></div>
+  <div class="input-group">
+  <span class="input-group-addon">Data wpisania</span>
+  <input type="date" id="date_from"  name="date_from" class="form-control" placeholder="Podaj datę w formacie rrrr-mm-dd"></div>
+	 <div class="input-group">
+  <span class="input-group-addon">Oddział</span>
+  <select id="ward_id" name="ward_id" class="form-control" placeholder="Wpisz oddział" autocomplete="off"  data-toggle="popover" onClick=pop()  data-content="Wprowadź oddział">
+  
+  <option value="1">Dzieciecy</option>
+  <option value="2">Chirurgiczny</option>
+  
+  <select></div>
+  <div class="input-group">
+  <span class="input-group-addon">Kod karty<span style="color: red">*</span></span>
+  <input type="text" name="record_code" id="record_code" class="form-control" placeholder="Wpisz kod wizyty"  autocomplete="off" data-toggle="popover" onClick=pop()  data-content="Wprowadź rekord karty"></div>
+  
+  
+  ';}
+  $db->close();
+  ?>
+
+        <p><input type="submit" id="button1" name="button" class="btn btn-primary btn-lg" role="button" value=Przyjmij ></a></p>
+
+
+      
+         
+
+	</div>
+    </div> 
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  
+</html>
+
+
+
+
